f2c615252a4cfde765b8422938c4977a507954ff
Victor Torres <vt.alex.2001@gmail.com>
2022-08-18 17:40:35 -0500

    Created the intro, got basic events set up, and got the code ready for introduction of skills, crafting, and smithing. The village can also be expanded. Map still on back burner.

--- script.js
+++ script.js
@@ -30,6 +30,65 @@ let Rock = {
     Have: false
 }
 
+let R_Berries = {
+    id: 4,
+    Name:"Red Berries",
+    Description: "Round, bright red, and sweet.",
+    Category: "Material",
+    Rarity: 0,
+    Amount: 0,
+    Have: false
+}
+
+let B_Berries = {
+    id: 5,
+    Name:"Blue Berries",
+    Description: "Round, dull blue, and sour.",
+    Category: "Material",
+    Rarity: 0,
+    Amount: 0,
+    Have: false
+}
+
+let P_Berries = {
+    id: 6,
+    Name:"Purple Berries",
+    Description: "Round, deep purple, and sickening to eat.",
+    Category: "Material",
+    Rarity: 0,
+    Amount: 0,
+    Have: false
+}
+
+let BL_Berries = {
+    id: 7,
+    Name:"Black Berries",
+    Description: "Round, dark black, and bitter.",
+    Category: "Material",
+    Rarity: 0,
+    Amount: 0,
+    Have: false
+}
+
+let Twine = {
+    id: 8,
+    Name:"Twine",
+    Description: "Like string, too weak to really use.",
+    Category: "Material",
+    Rarity: 0,
+    Amount: 0,
+    Have: false
+}
+
+let Leech_Blood = {
+    id: 9,
+    Name:"Leech Blood",
+    Description: "It's gross, it's icky, but may be of some use",
+    Category: "Material",
+    Rarity: 0,
+    Amount: 0,
+    Have: false
+}
 /*^Items^*/
 /*vEntitiesv*/
 
@@ -43,17 +102,21 @@ let none = {
     luck: 1
 }
 
+none.health = none.endurance*15;
+none.current_health = none.health;
+
 let You = {
     name: "Alex",
     level: 1,
     title: "The Starter",
-    strength: 500,
+    strength: 15,
     endurance: 20,
-    agility: 500,
+    agility: 5,
     luck: 3,
     EXP: 0,
     EXPneed: "",
-    AP: 0
+    AP: 0,
+    dead: false
 };
 
 You.health = You.endurance*15;
@@ -69,41 +132,74 @@ let Slime = {
     endurance: 60,
     agility: 3,
     luck: 0,
-    EXP: 10000,
+    EXP: 150,
     state: "alive",
     drop: [[Slime_Blob, 2, 2500],
             [Twig, 1, 500],
             [Rock, 1, 250]]
 };
 
+
+let Leech = {
+    name: "Leech",
+    level: 1,
+    title: "Sucker",
+    health: 15,
+    current_health: 15,
+    strength: 1,
+    endurance: 10,
+    agility: 1,
+    luck: 0,
+    EXP: 10,
+    state: "alive",
+    drop: [[Leech_Blood, 1, 2500],
+            [Rock, 1, 250]]
+};
 Slime.health = Slime.endurance*15;
 Slime.current_health = Slime.health;
 
 /*^Entities^*/
 /*vLocationsv*/
+let Bed = {
+    name: "Bed - Home",
+    options: [["Wake up", "Awaken"]],
+    description: "zzz"
+}
+
+let Home_Bedroom = {
+    name: "Bedroom - Home",
+    options: [["Leave Bedroom", "Home"], ["Go to bed", "Sleep"]],
+    description: "Your small little bedroom, a nice bed and well not much, yet. Your personal bookcase sits in the corner."
+}
 
 let Home_Living_Room = {
-    name: "Living Room",
-    enemies: [none]
+    name: "Living Room - Home",
+    options: [["Go Outside", "Village"], ["Grab your map", "MAP"], ["Go to Bedroom", "Home Bedroom"]],
+    description: "The Living room to your home, there's a nice comfy chair and a book case."
 }
 
-let Outside_Main = {
-    name: "Outside",
-    enemies: [none]
+let Village_center = {
+    name: "Village Center",
+    options: [["Go to Forest", "Forest"], ["Go Home", "Home"]],
+    description: "The center of your little village. People moving quickly to do their jobs to eat another day."
 }
 
-let Forest = {
-    name: "Living Room",
-    enemies: [[Slime, 100]]
+let Forest_Entrance = {
+    name: "Forest Entrance",
+    options: [["Leave Forest", "Village"], ["Forage for resources", "Forest-gather-easy"]],
+    description: "The Entrance to the forest surrounding your village."
 }
 
-Home_Living_Room = {options: [["Go Outside", Outside_Main]]};
-Outside_Main = {options: [["Go to Forest", Forest], ["Go Home", Home_Living_Room]]};
-Forest = {options: [["Leave Forest", Outside_Main]]};
+let Forest_Gathering = {
+    name: "Forest Foilage",
+    options: [["Stop Gathering", "Forest-gather-easy-end"]],
+    description: "Foilage surrounds you as you gather, you hear noises"
+}
+
 /*^Locations^*/
 
-char_location = Home_Living_Room;
-enemy = char_location.enemies[0];
+char_location = Home_Bedroom;
+enemy = none;
 
 let msglogcount = 0;
 let bbcount = 1;
@@ -118,7 +214,13 @@ let day = 16;
 let hour = 0;
 let minute = 0;
 
+let gathering = false;
+let fighting = false;
+let healing = false;
+
 window.onload = function () {
+    intro();
+
     time();
     InfoReset(You);
     InfoReset(enemy);
@@ -130,30 +232,25 @@ window.onload = function () {
     LUCKtooltip();
 
     document.getElementById("season_weather").innerHTML = "[" + season + "] [" + weather + "]";
+    document.getElementById("Location").innerHTML = char_location.name;
+
+    chooseopt();
 
     document.getElementById('STR').onclick = STRstatup;
     document.getElementById('END').onclick = ENDstatup;
     document.getElementById('AGL').onclick = AGLstatup;
     document.getElementById('LUCK').onclick = LUCKstatup;
 
-    document.getElementById('Battle_Start').onclick = fight;
-    document.getElementById('Heal').onclick = heal;
-    
+    document.getElementById('MAP').style.visibility = 'hidden';
 };
 
 
 
 async function fight() {
-    document.getElementById("Heal").disabled = true;
-    document.getElementById("Battle_Start").disabled = true;
+    if (enemy == none) {return;}
+    InfoReset(enemy);
     console.log("Fight");
-    if (enemy.state == "dead") {
-        console.log("Enemy revived");
-        enemy.current_health = enemy.health;
-        document.getElementById('enemy_health').innerHTML = hp(enemy);
-        enemy.state = "alive"
-    }
-    while (enemy.current_health  > 0 && You.current_health > 0) {
+    while (enemy.current_health  > 0 && You.current_health > 0 && fighting == true) {
         enemy.current_health = enemy.current_health - hit(You);
         document.getElementById('enemy_health').innerHTML = hp(enemy);
         await sleep(250);
@@ -164,30 +261,31 @@ async function fight() {
         } 
     }
     
-   
-    if (enemy.current_health <= 0) {
+    if (fighting == false) {
+        enemy = none;
+        InfoReset(enemy);
+        return;
+    } else if (enemy.current_health <= 0) {
         enemy.state = "dead"
         loot(enemy.drop);
         EXPdrop(enemy);
         mbar("You win");
-        document.getElementById("Heal").disabled = false;
-        document.getElementById("Battle_Start").disabled = false;
+        fighting = false;
     } else if (You.current_health <= 0) {
         mbar("You lose");
-        document.getElementById("Heal").disabled = false;
-        document.getElementById("Battle_Start").disabled = false;
+        doact("Sleep")
+        You.dead = true;
+        fighting = false;
     } else {
         mbar("ERROR");
-        document.getElementById("Heal").disabled = false;
-        document.getElementById("Battle_Start").disabled = false;
+        fighting = false;
     }
 }
 
 async function heal() {
-    document.getElementById("Battle_Start").disabled = true;
-    document.getElementById("Heal").disabled = true;
     console.log("heal")
-    while (You.current_health < You.health) {
+    while (You.current_health < You.health && healing == true) {
+        if (You.current_health > 0) {You.dead = false};
         You.current_health = (You.current_health + You.endurance);
         if (You.current_health > You.health) {
             You.current_health = You.health;
@@ -195,27 +293,23 @@ async function heal() {
         document.getElementById('your_health').innerHTML = hp(You);
         await sleep(1000);
     }
-    document.getElementById("Battle_Start").disabled = false;
-    document.getElementById("Heal").disabled = false;
 }
 
 function title(entity) {
     return entity.name + " " + "'" + entity.title + "'" + " " + "Lv." + entity.level;
-};
-
+}
 
 function hp(entity) {
     return entity.current_health + " / " + entity.health;
-};
-
+}
 
 function stats(entity) {
     return "STR:" + entity.strength + " END:" + entity.endurance + " AGL:" + entity.agility + " LUCK:" + entity.luck;
-};
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
- }
+}
 
  function crit(entity) {
     if (entity.luck > 0) {
@@ -227,7 +321,7 @@ function sleep(ms) {
     } else {
         return [1, false];
     }
- }
+}
 
  function mbar (msg, css) {
     if (msglogcount >= 19) {
@@ -279,9 +373,12 @@ function EXP () {
 
 function add_inventory (itemTable) {
     var item = itemTable[0];
-    var quantity = itemTable[1];
-    item.Amount = item.Amount + Math.round(quantity);
-    var msg = item.Name + " " + "x" + item.Amount;
+    var quantity = Math.round(Math.random() * (itemTable[1]));
+    if (quantity < 1) {
+        quantity = 1;
+    }
+    item.Amount = item.Amount + quantity;
+    var msg = "&nbsp;" + item.Name + " " + "x" + item.Amount;
     if (bbcount % 2 == 0) {
         column = "backpack_column_2";
     } else if (bbcount % 2 == 1) {
@@ -381,7 +478,7 @@ function STRtooltip () {
     });
     
     document.getElementById('STR').addEventListener("mouseout", function () {document.getElementById("STR_tooltip").style.display = "none"});
-    }
+}
 
     /*END Tooltip*/ 
 function ENDtooltip () {
@@ -396,7 +493,7 @@ function ENDtooltip () {
     });
     
     document.getElementById('END').addEventListener("mouseout", function () {document.getElementById("END_tooltip").style.display = "none"});
-    }
+}
 
     /*AGL Tooltip*/ 
 function AGLtooltip () {
@@ -411,7 +508,7 @@ function AGLtooltip () {
     });
     
     document.getElementById('AGL').addEventListener("mouseout", function () {document.getElementById("AGL_tooltip").style.display = "none"});
-    }
+}
 
     /*LUCK Tooltip*/ 
 function LUCKtooltip () {
@@ -426,10 +523,9 @@ function LUCKtooltip () {
     });
     
     document.getElementById('LUCK').addEventListener("mouseout", function () {document.getElementById("LUCK_tooltip").style.display = "none"});
-    }
+}
 
-
-    function statup (stat) {
+function statup (stat) {
         switch (stat) {
             default:
                 console.log("Statup function Error");
@@ -459,33 +555,33 @@ function LUCKtooltip () {
                 InfoReset(You);
                 break;
         }
-    }
+}
 
-    function STRstatup () {
+function STRstatup () {
         if (You.AP >= 1){ 
             statup("STR");
         }
-    }
+}
 
-    function ENDstatup () {
+function ENDstatup () {
         if (You.AP >= 1){ 
             statup("END");
         }
-    }
+}
 
-    function AGLstatup () {
+function AGLstatup () {
         if (You.AP >= 1){ 
             statup("AGL");
         }
-    }
+}
 
-    function LUCKstatup () {
+function LUCKstatup () {
         if (You.AP >= 5){ 
             statup("LUCK");
         }
-    }
+}
 
-    async function time () {
+async function time () {
         while (true) {
             let date = year + "/" + month + "/" + day;
             let clock = ((hour<10?'0':'') + hour) + ':' + ((minute<10?'0':'') + minute);
@@ -548,9 +644,9 @@ function LUCKtooltip () {
             }
             await sleep(1000);
         }       
-    }
+}
 
-    function weatherfunc () {
+function weatherfunc () {
         let chance;
         switch (season) {
             default:
@@ -1845,4 +1941,181 @@ function LUCKtooltip () {
                 }
                  break;
         }
+}
+
+function chooseopt () {
+        document.getElementById("header_action_box").innerHTML = char_location.description;
+        var options_amount = char_location.options.length;
+        while (document.getElementById("action_main_box").firstChild) {
+            document.getElementById("action_main_box").removeChild(document.getElementById("action_main_box").firstChild);
+          }
+        for (let i = 0; i <= options_amount - 1; i++){
+            var bar = document.createElement("button");
+            bar.innerHTML = char_location.options[i][0];
+            bar.classList.add("abar");
+            bar.id = "a_butt"
+            bar.value = char_location.options[i][1];
+            document.getElementById("action_main_box").appendChild(bar);
+        }
+        
+        document.querySelectorAll(".abar").forEach(box => {box.addEventListener('click', (e) => {doact(e.target.value)});});
+}
+
+async function doact (value) {
+    switch (value) {
+        default:
+            console.log("Do action error code: " + value);
+            break;
+        case "Village":
+            char_location = Village_center;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            break;
+        case "Home":
+            char_location = Home_Living_Room;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            break;
+        case "Forest":
+            char_location = Forest_Entrance;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            break;
+        case "MAP":
+            document.getElementById('MAP').style.visibility = 'visible';
+            Home_Living_Room.options = Home_Living_Room.options.filter(el => el[1] != "MAP");
+            mbar("You found your map!")
+            chooseopt();
+            break;
+        case "Forest-gather-easy":
+            char_location = Forest_Gathering;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            while (char_location == Forest_Gathering) {
+            Gathering([[Twine, 5, 350],[Twig, 4, 300],[Rock, 5, 250],[R_Berries, 1, 200],[B_Berries, 1, 150],[BL_Berries, 1, 100],[P_Berries, 1, 50]]);
+            while(gathering == true) {
+                if(Math.random() * 2000 > 1955) {
+                    gathering = false;
+                    enemy = Leech;
+                    fighting = true;
+                    fight();
+                    while (enemy.state == "alive" && You.dead == false) {
+                        await sleep(1000);
+                    }
+                    console.log("not sleeping");
+                    if(enemy.state == "dead") {
+                        enemy = none;
+                        gathering = true;
+                        InfoReset(enemy)
+                    }
+                    console.log(gathering)
+                }
+                await sleep (1500);
+            }
+            }
+            break;
+        case "Forest-gather-easy-end":
+            char_location = Forest_Entrance;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            gathering = false;
+            fighting = false;
+            break;
+        case "Sleep":
+            char_location = Bed;
+            gathering = false;
+            fighting = false;
+            healing = true;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            heal();
+            break;
+        case "Awaken":
+            char_location = Home_Bedroom;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            healing = false;
+            break;
+        case "Home Bedroom":
+            char_location = Home_Bedroom;
+            document.getElementById("Location").innerHTML = char_location.name;
+            chooseopt();
+            break;
     }
+}
+
+async function Gathering (loot) {
+    gathering = true;
+    while(gathering == true) {
+        console.log("Gathering");
+        var items = loot.length;
+        for (let i = 0; i <= items - 1; i++){
+            if (loot[i][2] >= Math.random()*10000) {
+                add_inventory(loot[i]);
+            }
+        }
+        await sleep(1500);
+    }
+    
+}
+
+function togglealloff () {
+        document.getElementById('header').style.visibility = 'hidden';
+        document.getElementById('inv').style.visibility = 'hidden';
+        document.getElementById('combat_box').style.visibility = 'hidden';
+        document.getElementById('enemy_box').style.visibility = 'hidden';
+        document.getElementById('the_log').style.visibility = 'hidden';
+        document.getElementById('action_box').style.visibility = 'hidden';
+        document.getElementById('bookshelf').style.visibility = 'hidden';
+}
+
+async function intro_message (message) {
+    let msg;
+    switch (message) {
+        default:
+            return;
+        case 1:
+            msg = "You wake up from a deep sleep.<br><br> After working at your father's forge for so long you have learned to sleep through anything.<br><br> But this was hard to sleepthrough, yesterday you came home with some groceries your dad had you get.<br><br> You came home and he was gone, the place a mess, no note, nothing...<br><br> You cannot dwell on this for too long.<br><br> You have to survive, you have to get stronger, and you have to find him.<br><br> One small problem.<br><br> You only know how to make and use axes.<br><br> Guess you better get to work.";
+            document.getElementById('intro_message_box').style.height = 'auto';
+            break;
+        case 2:
+            msg = "You get up check yourself and grab your bag.";
+            document.getElementById('inv').style.visibility = 'visible';
+            break;
+        case 3:
+            msg = "You jump a little, and punch the air, getting loosened up.";
+            document.getElementById('combat_box').style.visibility = 'visible';
+            document.getElementById('enemy_box').style.visibility = 'visible';
+            break;
+        case 4:
+            msg = "Your mind clears as you begin to remember what is happening.";
+            document.getElementById('the_log').style.visibility = 'visible';
+            break;
+        case 5:
+            msg = "You look to the wall seing an empty bookshelf. Your father always said reading and books were the best way learn and store what you know.";
+            document.getElementById('bookshelf').style.visibility = 'visible';
+            break;
+        case 6:
+            msg = "You blink and look around your room as you feel yourself ready to go.";
+            document.getElementById('intro_message_box').innerHTML = msg;
+            document.getElementById('action_box').style.visibility = 'visible';
+            document.getElementById('header').style.visibility = 'visible';
+            document.getElementById('intro_message_box').style.opacity = '0';
+            await sleep(5000)
+            document.getElementById('intro_message_box').remove();
+            break;
+    }
+    document.getElementById('intro_message_box').innerHTML = msg;
+}
+
+async function intro() {
+    togglealloff();
+    var message_numb = 1;
+    document.getElementById('blackscreen').style.opacity = '0';
+    document.getElementById('intro_message_box').style.opacity = '1';
+    await sleep(5000)
+    document.getElementById('blackscreen').remove();
+    intro_message(message_numb);
+    document.getElementById('intro_message_box').addEventListener('click', () => {message_numb++; intro_message(message_numb);});
+}
+
